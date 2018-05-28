eef56442757add47f320898ca16b195dc28443b6
pocc <whim42+github@gmail.com>
2018-05-28 02:47:05 -0700

    Extension works when you go to chrome://extensions and use 'LOAD UNPCAKED'
    Right now, functionality is that it recognizes an IPv4 and IPv6 address
    and prepends the letters "US|" to it

---
+++ extension/content_script.js
@@ -0,0 +1,49 @@
+walk(document.body);
+
+function walk(node) 
+{
+	// Stolen from Stack Overflow: http://is.gd/mwZp7E	
+
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+            if(node.parentElement.tagName.toLowerCase() != "script") {
+                handleText(node);
+            }
+			break;
+	}
+}
+
+function handleText(textNode) {
+	var v = textNode.nodeValue;
+	// Encompasses both IPv4 and IPv6
+	var ip_regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])|^(?:^|(?<=\s))(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(?=\s|$)$"/g
+	
+	v = v.replace(ip_regex, function add_flag(match) {
+    // geoiplookup chosen because it's https and accepts both IPv4 (8.8.8.8) and IPv6 (2001:4860:4860::8888)
+		geoip_xml = get_geoip('https://json.geoiplookup.io/' + match)
+
+		var countrycode = "US|" //geoip_xml.getElementsByTagName("countrycode")[0];
+		return countrycode + match;
+	});
+
+	// Send value back to walk
+	textNode.nodeValue = v;
+} 
+
+function get_geoip(){}
+// Look here to parse JSON: https://stackoverflow.com/questions/2499567/how-to-make-a-json-call-to-a-url/2499647#2499647
